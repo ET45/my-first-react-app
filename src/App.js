a264d44dfc1522e6abcc0fe2636be4eba54d17b6
ET45 <emre.tugutlu@gmail.com>
2022-01-24 15:48:00 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Codaisseur
         </a>
         <p>Hello World</p>
         <p>Its cool</p>
